3d6fd3696995a0de11a36e1ab7925c3ddc4cf25d
akshyyyt <akshittomar5@gmail.com>
2026-04-19 01:20:00 +0530

    react router setup and landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero';
+import TrendingGames from '../components/TrendingGames';
+import TrendingSports from '../components/TrendingSports';
+
+const LandingPage = () => {
+  return (
+    <>
+      <Hero />
+      <TrendingGames />
+      <TrendingSports />
+    </>
+  );
+};
+
+export default LandingPage;
