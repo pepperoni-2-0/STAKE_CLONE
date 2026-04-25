a1f1bedf9eaf32c4c7c4adff2295482fa6078e7a
Arshnoor Singh <arshnoor1607@gmail.com>
2026-04-25 23:31:38 +0530

    feat: replace Dragon Tower and Whack-A-Mole card assets

--- src/Components/TrendingGames.jsx
+++ src/Components/TrendingGames.jsx
@@ -2,11 +2,11 @@ import { useNavigate } from 'react-router-dom';
 import './TrendingGames.css';
 
 const games = [
-  { id: 1, name: 'Mines', players: '1,141', slug: 'mines', img: '/assets/mines_logo.png' },
-  { id: 2, name: 'Dragon Tower', players: '2,302', slug: 'dragon-tower', img: 'https://mediumrare.imgix.net/2c3e16f0a3b8cd8d979265e48dd6a169937a4a4d0acb05ad532ca8345a1e6f21?w=180&amp;h=236&amp;fit=min&amp;auto=format' },
-  { id: 3, name: 'Coin Toss', players: '890', slug: 'coin-toss', img: '/assets/coinToss.png' },
-  { id: 4, name: 'Whack-A-Mole', players: '1,560', slug: 'whack-a-mole', img: 'https://mediumrare.imgix.net/5e6f7bb02df67a02a9182aab05d0976a9abbac7f45997975eed765332a8b7d73?w=180&amp;h=236&amp;fit=min&amp;auto=format' },
-  { id: 5, name: 'RPS Duel', players: '3,412', slug: 'rps', img: '/assets/rps.png' },
+  { id: 1, name: 'Mines', players: '1,141', slug: 'mines', img: '/assets/mines_logo.png', bgSize: 'contain' },
+  { id: 2, name: 'Dragon Tower', players: '2,302', slug: 'dragon-tower', img: '/assets/dragontower.png', bgSize: 'cover' },
+  { id: 3, name: 'Coin Toss', players: '890', slug: 'coin-toss', img: '/assets/coinToss.png', bgSize: 'contain' },
+  { id: 4, name: 'Whack-A-Mole', players: '1,560', slug: 'whack-a-mole', img: '/assets/whack.png', bgSize: 'cover' },
+  { id: 5, name: 'RPS Duel', players: '3,412', slug: 'rps', img: '/assets/rps.png', bgSize: 'contain' },
 ];
 
 const TrendingGames = () => {
@@ -27,7 +27,7 @@ const TrendingGames = () => {
             className="game-card"
             onClick={() => navigate(`/games/${game.slug}`)}
           >
-            <div className="game-img" style={{ backgroundImage: `url(${game.img})` }}></div>
+            <div className="game-img" style={{ backgroundImage: `url(${game.img})`, backgroundSize: game.bgSize || 'contain' }}></div>
             <div className="game-info">
               <div className="player-count">
                 <span className="dot"></span> {game.players} playing
